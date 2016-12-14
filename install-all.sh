8739f1c4a52407f0096a2e3aceafaffd59c49dcc
David Ben Knoble <ben.knoble@gmail.com>
2016-12-14 14:33:02 -0500

    Base installer
    Uses find to execute all scripts in the installers directory.
    May be prone to attack; use with caution.

---
+++ install-all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# an install script to install custom stuff
+# executes all scripts in ./installers/, which makes it prone to attack if not careful.
+# however, it also allows you to add your own installer scripts
+
+find ./installers/*.sh -maxdepth 1 -exec {} \;
