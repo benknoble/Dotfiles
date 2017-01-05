1c6a7bb067cfa1684e337d7d3e61c0eb1674b9d6
David Ben Knoble <ben.knoble@gmail.com>
2017-01-05 00:32:46 -0500

    Fix capitalization of update message.

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 # updates Dotfiles
 
 (cd ~/Dotfiles && git checkout master && git pull origin master) > /dev/null 2>&1 && echo "Updated" || echo "Update failed"
-echo "type 'reload' to reload updates"
+echo "Type 'reload' to reload updates"
