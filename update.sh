c028dba94577e16dd4a52d1ec60d4e2a0feb52da
David Ben Knoble <ben.knoble@gmail.com>
2017-01-05 00:34:38 -0500

    Make update message more verbose (include logout/in to see change

--- update.sh
+++ update.sh
@@ -3,3 +3,4 @@
 
 (cd ~/Dotfiles && git checkout master && git pull origin master) > /dev/null 2>&1 && echo "Updated" || echo "Update failed"
 echo "Type 'reload' to reload updates"
+echo "You may need to logout of the terminal and login for changes to take full effect"
