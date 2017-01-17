fa77c7367d6c8b1da0e7b2fa599e8a460124f55f
David Ben Knoble <ben.knoble@gmail.com>
2017-01-16 22:13:30 -0500

    Moves color definitions into colors.bash
    Part of reorganization. This allows the colors to be sourced in other
    ways than just including them in the login script.

---
+++ colors.bash
@@ -0,0 +1,50 @@
+#!/bin/bash
+#color definitions
+
+########################################
+#         COLOR DEFINITIONS            #
+########################################
+
+CS='\['
+CE='\]'
+
+export NC='\033[0m'
+
+export BLACK='\033[0;30m'
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export WHITE='\033[0;37m'
+
+#bold
+export BBLACK='\033[0;90m'
+export BRED='\033[0;91m'
+export BGREEN='\033[0;92m'
+export BYELLOW='\033[0;93m'
+export BBLUE='\033[0;94m'
+export BPURPLE='\033[0;95m'
+export BCYAN='\033[0;96m'
+export BWHITE='\033[0;97m'
+
+#background
+export ON_BLACK='\033[0;40m'
+export ON_RED='\033[0;41m'
+export ON_GREEN='\033[0;42m'
+export ON_YELLOW='\033[0;43m'
+export ON_BLUE='\033[0;44m'
+export ON_PURPLE='\033[0;45m'
+export ON_CYAN='\033[0;46m'
+export ON_WHITE='\033[0;47m'
+
+#bold background
+export ON_BBLACK='\033[0;100m'
+export ON_BRED='\033[0;101m'
+export ON_BGREEN='\033[0;102m'
+export ON_BYELLOW='\033[0;103m'
+export ON_BBLUE='\033[0;104m'
+export ON_BPURPLE='\033[0;105m'
+export ON_BCYAN='\033[0;106m'
+export ON_BWHITE='\033[0;107m'
