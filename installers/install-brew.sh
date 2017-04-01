297b325fbe3a5bedfabda90fede5bda5544e6876
David Ben Knoble <ben.knoble@gmail.com>
2017-04-01 15:04:10 -0400

    Add python/3 and tree to installers

--- installers/install-brew.sh
+++ installers/install-brew.sh
@@ -83,6 +83,33 @@ if [[ $installBrew =~ ^(y|Y) ]]; then
     else
         echo "Skipping nethack..."
     fi
+
+    #install python
+    read -n 1 -p "Install python via Brew [Y/n]? " installPython && echo
+    if [[ $installPython =~ ^(y|Y) ]]; then
+        echo "Installing Python..."
+        (brew install python) || echo "Brew/python installation failed"
+    else
+        echo "Skipping python..."
+    fi
+
+    #install python3
+    read -n 1 -p "Install python3 via Brew [Y/n]? " installPython3 && echo
+    if [[ $installPython3 =~ ^(y|Y) ]]; then
+        echo "Installing python3..."
+        (brew install python3) || echo "Brew/python3 installation failed"
+    else
+        echo "Skipping python3..."
+    fi
+
+    #install tree
+    read -n 1 -p "Install tree via Brew [Y/n]? " installTree && echo
+    if [[ $installTree =~ ^(y|Y) ]]; then
+        echo "Installing tree..."
+        (brew install tree) || echo "Brew/tree installation failed"
+    else
+        echo "Skipping tree..."
+    fi
 else
     echo "Skipping Brew..."
 fi
