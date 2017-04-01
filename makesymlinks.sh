7ea976b78c348d66dda1885234f3afae2f7bebf0
David Ben Knoble <ben.knoble@gmail.com>
2017-04-01 15:08:31 -0400

    Make symlink script more understandable
    Pauses between command execution gives the user a chance to see what's
    going on.
    
    And additional echo or two give more information, and blank lines are
    always nice.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -15,17 +15,28 @@ files="bash_profile vimrc bashrc"    # list of files/folders to symlink in homed
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing Dotfiles in ~"
 mkdir -p $olddir
+sleep 2
 echo "...done"
+echo
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
+sleep 2
 echo "...done"
+echo
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
     echo "Moving .$file from ~ to $olddir"
     mv ~/.$file $olddir/$file
+    sleep 2
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
+    sleep 2
+    echo "Finished $file"
+    sleep 2
+    echo
 done
+
+echo "...done"
