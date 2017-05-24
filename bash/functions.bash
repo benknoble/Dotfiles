c7b78578e63ec4ad08a1ae0a9f0bd4024645951e
David Ben Knoble <ben.knoble@gmail.com>
2017-05-24 17:32:57 -0400

    [Closes #21] Internally change displayPath
    Output is no different (newline separated path entries), but the
    internals makes better use of parameter expansion than the previous
    version did.

--- bash/functions.bash
+++ bash/functions.bash
@@ -6,7 +6,7 @@ trash () { mv "$@" ~/.Trash ; }              # Move a file to Trash
 aliases () { cat ~/Dotfiles/bash/aliases.bash | tail -n +4 | less ; }                 # List all aliases found in this file
 functions () { cat ~/Dotfiles/bash/functions.bash | tail -n +4 | less ; }             # List all functions found in this file
 findPid () { lsof -t -c "$@" ; }             # Find Pid of specified process
-displayPath () { for path in "${PATH//:/ }"; do echo "$path"; done; }                 # Print path separated by newlines
+displayPath () { echo "${PATH//:/$'\n'}" ; } # Print path separated by newlines
 mkcd () { mkdir "$@" && cd "$@" ; }          # mkdir and cd
 editall () { vim -p "$@" ; }                 # edit all files provided as arguments in vim tabs
 extract () {                                 # attempt to extract file with correct extraction method
