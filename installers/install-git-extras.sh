f1f915e0aac63c467a68c70b559c615b77121907
David Ben Knoble <ben.knoble@gmail.com>
2017-07-14 21:57:23 -0400

    [Closes #25] Install hooks and tree hook
    Install hooks, so now I can add them as I see fit.
    Additionally, contains the tree hook from commit 2e8b644.

--- installers/install-git-extras.sh
+++ installers/install-git-extras.sh
@@ -8,5 +8,23 @@ echo "Previous git extras (git prompt and git completion) are now included by
 installing git via brew and will be sourced if installed; otherwise, they will
 not be used."
 echo
+echo "Setting up git hooks..."
+echo
+
+echo "Removing hooks in .git/hooks..."
+[[ -e .git/hooks ]] && {
+    [[ -L .git/hooks ]] && { rm -fv .git/hooks; }
+    [[ -d .git/hooks ]] && { rm -fv .git/hooks/*; rmdir .git/hooks; }
+}
+echo "...done removing hooks"
+echo
+
+echo "Linking in hooks..."
+ln -Fivs ~/Dotfiles/hooks/ .git/
+echo "...done linking in hooks"
+echo
+
+echo "...done with git hooks"
+echo
 echo "...done with git extras"
 echo
