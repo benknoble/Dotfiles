735f3493add9d3edcd090b4d4f4a9d32c27f7cae
David Ben Knoble <ben.knoble@gmail.com>
2017-07-14 20:32:36 -0400

    Correct extract function
    Previous versions tried to use `rar` on rarchives and failed. Now use
    `unrar`.

--- bash/functions.bash
+++ bash/functions.bash
@@ -15,7 +15,7 @@ extract () {                                 # attempt to extract file with corr
             *.tar.bz2)  tar xjf "$1"      ;;
             *.tar.gz)   tar xzf "$1"      ;;
             *.bz2)      bunzip2 "$1"      ;;
-            *.rar)      rar x "$1"        ;;
+            *.rar)      unrar x "$1"      ;;
             *.gz)       gunzip "$1"       ;;
             *.tar)      tar xf "$1"       ;;
             *.tbz2)     tar xjf "$1"      ;;
