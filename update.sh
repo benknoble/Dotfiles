f4a3e79176455c73e0b263eb80e4cec7ca6f9b4b
David Ben Knoble <ben.knoble@gmail.com>
2017-09-09 16:44:59 -0400

    Move brew check to appropriate spot in update
    Fixes two problems:
    1. Updating on a non-brew system exited with failure status
    2. The output of brew bundle check would come after the reload reminder

--- update.sh
+++ update.sh
@@ -4,6 +4,6 @@
 (cd ~/Dotfiles \
   && git checkout master \
   && git pull origin master) && echo "Updated" || echo "Update failed"
+which brew > /dev/null 2>&1 && brew bundle check
 echo "Type 'reload' to reload updates"
 echo "You may need to logout of the terminal and login for changes to take full effect"
-which brew > /dev/null 2>&1 && brew bundle check
