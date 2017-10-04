e77b65602e1819fa756416deaf10f207fc14ea90
David Ben Knoble <ben.knoble@gmail.com>
2017-10-04 11:24:27 -0400

    Allow extract to operate on multiple files
    Fixes #100

--- bash/functions.bash
+++ bash/functions.bash
@@ -10,23 +10,25 @@ displayPath () { echo "${PATH//:/$'\n'}" ; } # Print path separated by newlines
 mkcd () { mkdir "$@" && cd "$@" ; }          # mkdir and cd
 editall () { vim -p "$@" ; }                 # edit all files provided as arguments in vim tabs
 extract () {                                 # attempt to extract file with correct extraction method
-  if [ -f "$1" ]; then
-    case "$1" in
-      *.tar.bz2)  tar xjf "$1"      ;;
-      *.tar.gz)   tar xzf "$1"      ;;
-      *.bz2)      bunzip2 "$1"      ;;
-      *.rar)      unrar x "$1"      ;;
-      *.gz)       gunzip "$1"       ;;
-      *.tar)      tar xf "$1"       ;;
-      *.tbz2)     tar xjf "$1"      ;;
-      *.tgz)      tar xzf "$1"      ;;
-      *.zip)      unzip "$1"        ;;
-      *.Z)        uncompress "$1"   ;;
-      *)          echo "'$1' cannot be extracted via extract()" ;;
-    esac
-  else
-    echo "'$1' is not a valid file"
-  fi
+  for file in "$@"; do
+    if [ -f "$file" ]; then
+      case "$file" in
+        *.tar.bz2)  tar xjf "$file"      ;;
+        *.tar.gz)   tar xzf "$file"      ;;
+        *.bz2)      bunzip2 "$file"      ;;
+        *.rar)      unrar x "$file"      ;;
+        *.gz)       gunzip "$file"       ;;
+        *.tar)      tar xf "$file"       ;;
+        *.tbz2)     tar xjf "$file"      ;;
+        *.tgz)      tar xzf "$file"      ;;
+        *.zip)      unzip "$file"        ;;
+        *.Z)        uncompress "$file"   ;;
+        *)          echo "'$file' cannot be extracted via extract()" ;;
+      esac
+    else
+      echo "'$file' is not a valid file"
+    fi
+  done
 }
 
 pathadd() {                                 # add to path
