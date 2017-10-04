0299230fb51b4dd439b251ab05b9b5a8a04d1318
David Ben Knoble <ben.knoble@gmail.com>
2017-10-04 11:49:25 -0400

    Add command not found handle, join_by function
    Closes #97
    
    Requires a documentation update

--- bash/functions.bash
+++ bash/functions.bash
@@ -52,3 +52,13 @@ getip() {
 quote() {
   printf %s\\n "$1" | sed "s/'/'\\\\''/g;1s/^/'/;\$s/\$/'/"
 }
+
+# joinby delim args
+join_by() {
+  local d=$1
+  shift
+  echo -n "$1"
+  shift
+  printf "%s" "${@/#/$d}"
+  echo
+}
