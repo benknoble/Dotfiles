3d81cfa6091ca4cd66238d814752f1ca5b2e9ff8
David Ben Knoble <ben.knoble@gmail.com>
2017-10-07 21:15:10 -0400

    Use /usr/bin/env bash to be slightly more portable
    Closes #40

--- setup/installers/install-git-extras.sh
+++ setup/installers/install-git-extras.sh
@@ -1,4 +1,4 @@
-#! /bin/bash -
+#! /usr/bin/env bash
 # installer script for git extras
 
 echo
--- setup/makesymlinks.sh
+++ setup/makesymlinks.sh
@@ -1,4 +1,4 @@
-#! /bin/bash -
+#! /usr/bin/env bash
 ############################
 # .make.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/Dotfiles
