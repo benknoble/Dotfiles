03c3337f4bbce0ba837b07aa4257ab3bd0487a61
David Ben Knoble <ben.knoble@gmail.com>
2017-12-09 23:24:55 -0500

    Refactor install-all to be functional, use builtin
    Find, while an excellent tool, can be substituted for bash globbing and
    conditionals in this case. This also allows me to have a slightly more
    robust implementation.

--- setup/install-all.sh
+++ setup/install-all.sh
@@ -8,8 +8,28 @@ set -euo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$(dirname "$DIR")/dotfiles-support"
 
+find_install_scripts() {
+  # find "$DIR"/installers -maxdepth 1 -type f -perm -u=x
+  for file in "$DIR"/installers/*.sh ; do
+    [[ -x "$file" ]] || continue
+    echo "$file"
+  done
+}
+
+execute_installer() {
+  display_message "Execute ${1##*setup/}?"
+  read -n 1 -p "[y/n]> " install && echo
+  if input_matches_yY "$install" ; then
+    "$1"
+  else
+    display_message "Skipping ${1##*setup/}"
+  fi
+}
+
 execute_installers() {
-  find "$DIR"/installers -maxdepth 1 -type f -name "*.sh" -ok sh {} \;
+  for file in $( find_install_scripts ) ; do
+    execute_installer "$file"
+  done
 }
 
 execute_installers
