8d1734b5d810e5b22d22d034fdcd7a35810bad79
David Ben Knoble <ben.knoble@gmail.com>
2018-02-15 19:50:50 +0100

    Remove hyphens from bash scripts

--- setup/install-all.sh
+++ setup/install-all.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env bash -
+#! /usr/bin/env bash
 # an install script to install custom stuff
 # executes all scripts in ./installers/, which makes it prone to attack if not careful.
 # however, it also allows you to add your own installer scripts
