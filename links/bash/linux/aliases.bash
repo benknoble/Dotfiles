65e6a193ce936933cfb87ed48581968486530eee
David Ben Knoble <ben.knoble@gmail.com>
2018-07-22 13:19:27 -0400

    Add linux aliases

---
+++ links/bash/linux/aliases.bash
@@ -0,0 +1,20 @@
+if [ -x /usr/bin/dircolors ]; then
+  if [[ -r ~/.dircolors ]]; then
+    eval "$(dircolors -b ~/.dircolors)"
+  else
+    eval "$(dircolors -b)"
+  fi
+  alias ls='ls -F --color=auto'
+  alias grep='grep --color=auto'
+  alias egrep='egrep --color=auto'
+  alias fgrep='fgrep --color=auto'
+fi
+
+alias lsa='ls -a'
+alias ll='ls -l'
+alias lla='ll -a'
+alias lh='ll -h'
+alias lha='lla -h'
+
+alias copy='xsel -bi'
+alias paste='xsel -bo'
