9692ae154994be04afda7eb133e292f470911484
David Ben Knoble <ben.knoble@gmail.com>
2018-11-26 14:45:37 -0500

    Simplify installer logic

--- setup/install-all.sh
+++ setup/install-all.sh
@@ -8,14 +8,6 @@ set -euo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$(dirname "$DIR")/dotfiles-support"
 
-find_install_scripts() {
-  # find "$DIR"/installers -maxdepth 1 -type f -perm -u=x
-  for file in "$DIR"/installers/*.sh ; do
-    [[ -x "$file" ]] || continue
-    echo "$file"
-  done
-}
-
 execute_installer() {
   display_message "Execute ${1##*setup/}?"
   read -n 1 -p "[y/n]> " install && echo
@@ -27,7 +19,8 @@ execute_installer() {
 }
 
 execute_installers() {
-  for file in $( find_install_scripts ) ; do
+  for file in "$DIR"/installers/*.sh ; do
+    [[ -x "$file" ]] || continue
     execute_installer "$file"
   done
 }
