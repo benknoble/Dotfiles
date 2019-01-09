9d949e601494c74ffba4cb818143a501d1452c9d
David Ben Knoble <ben.knoble@gmail.com>
2019-01-09 00:41:06 -0500

    Speed up bash init
    I've moved the two slowest pieces (travis and heroku completions) to
    functions I can invoke when I need them.
    
    Of course, building my prompt string is also rather slow. Such is life.

--- links/bash/functions.bash
+++ links/bash/functions.bash
@@ -117,3 +117,16 @@ EG() {
 L() {
   less "$@"
 }
+
+# heroku completion
+complete_heroku() {
+  command -v heroku &> /dev/null \
+    && eval "$(heroku autocomplete:script bash)"
+}
+
+# travis completion
+complete_travis() {
+  command -v travis &> /dev/null \
+    && [[ -f ~/.travis/travis.sh ]] \
+    && source ~/.travis/travis.sh
+}
