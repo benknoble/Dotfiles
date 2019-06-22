6c56a4d0671bb4c8ff846738b787cca844d90f90
David Ben Knoble <ben.knoble@gmail.com>
2019-06-22 14:15:00 -0400

    Add mandpdf function
    Credit to @jackmoody11

--- links/bash/mac/functions.bash
+++ links/bash/mac/functions.bash
@@ -3,3 +3,9 @@ getip() {
   ipconfig getifaddr en0
 }
 
+manpdf() {
+  local manpage="$1"
+  (( $# > 0 )) && shift
+  local manargs=( "$@" )
+  man -t "${manargs[@]}" "$manpage" | open -f -a Preview
+}
