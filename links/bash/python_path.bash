98fc47bdf7ff55da9e6f86054b974ef2df24d3f9
David Ben Knoble <ben.knoble@gmail.com>
2019-07-02 22:04:22 -0400

    bash: simplify python_path computation

--- links/bash/python_path.bash
+++ links/bash/python_path.bash
@@ -2,12 +2,7 @@
 
 if command -v python3 >/dev/null 2>&1 ; then
 
-  python_path="$(python3 - <<SNAKE
-import site
-import os.path as path
-print(path.join(site.USER_BASE, 'bin'))
-SNAKE
-)"
+  python_path="$(python3 -m site --user-base)/bin"
   pathadd "$python_path"
 
 fi
