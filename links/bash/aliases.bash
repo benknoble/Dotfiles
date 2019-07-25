e5f6bb0f044a60761ae611d07dc266029cc97ed2
David Ben Knoble <ben.knoble+github@gmail.com>
2019-07-24 20:04:11 -0400

    bash: remove unecessary aliases

--- links/bash/aliases.bash
+++ links/bash/aliases.bash
@@ -1,16 +1,12 @@
 #! /usr/bin/env bash
 # alias definitions
 
-alias quit='exit'                           # Because Mac OS X tends to call it quit
 alias cp='cp -iv'                           # Preferred 'cp' implementation
 alias mv='mv -iv'                           # Preferred 'mv' implementation
 alias mkdir='mkdir -pv'                     # Preferred 'mkdir' implementation
-alias hidden='ls -a | grep "^\."'           # List all hidden files
-alias ~='cd ~'                              # Shortcut to cd ~
 alias edit='$EDITOR'                        # Open a file
 alias explore='vim +Explore'                # Open vim's Explore mode in current directory
 alias starwars='telnet towel.blinkenlights.nl'      # Watch Star Wars
-alias beep="echo -n $'\a'"                  # Do a beep
 alias today='date -j +"%e %B %Y"'           # A format of the date
 alias uhtop='htop -u $USER'                 # Run htop for the current user's processes
 alias r='fc -s'                             # Rerun last command [with prefix]
