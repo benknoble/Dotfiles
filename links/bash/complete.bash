d45be7d050ddb26ab67a92c296a227e3942bcb6d
David Ben Knoble <ben.knoble+github@gmail.com>
2019-07-24 21:31:24 -0400

    bash: move some bashrc things into files
    Leave the brew variables in bashrc because we need them in a few places.

--- links/bash/complete.bash
+++ links/bash/complete.bash
@@ -10,3 +10,19 @@ complete_travis() {
     && [[ -f ~/.travis/travis.sh ]] \
     && source ~/.travis/travis.sh
 }
+
+_pip_completion()
+{
+  COMPREPLY=( $( COMP_WORDS="${COMP_WORDS[*]}" \
+    COMP_CWORD=$COMP_CWORD \
+    PIP_AUTO_COMPLETE=1 $1 ) )
+}
+complete -o default -F _pip_completion pip
+
+
+# vault completion
+vault_path="$(command -v vault)"
+if [[ -n "$vault_path" ]] ; then
+  complete -C "$vault_path" vault
+fi
+unset vault_path
--- links/bash/paths.bash
+++ links/bash/paths.bash
@@ -18,3 +18,28 @@ if command -v python3 >/dev/null 2>&1 ; then
   python_path="$(python3 -m site --user-base)/bin"
   pathadd "$python_path"
 fi
+
+# Add bin folder for scripts to path
+pathadd "$HOME/.bin"
+
+if "$has_brew" ; then
+  pathadd_front "$brew_prefix/bin"
+  brew_fix() {
+    local type="$1"
+    local path="${!type}"
+    local prepend="$2"
+    case "$path" in
+      "$prepend"* ) true ;;
+      * ) eval export "$type=$prepend:$path" ;;
+    esac
+  }
+  # first clear manpath on macs
+  case "$OS" in
+    mac ) export MANPATH=
+  esac
+  brew_fix MANPATH "$brew_prefix/share/man"
+  brew_fix INFOPATH "$brew_prefix/share/info"
+fi
+
+# go
+[[ -d ~/go/bin ]] && pathadd ~/go/bin
