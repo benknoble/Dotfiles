6c79022f581cfa4dca9e57eb0413e37ee63ff6aa
David Ben Knoble <ben.knoble+github@gmail.com>
2019-07-24 20:59:29 -0400

    bash: split functions

--- links/bash/mac/productivity.bash
+++ links/bash/mac/productivity.bash
@@ -2,3 +2,13 @@
 alias finder='open -a Finder'
 # Open an application
 alias app='open -a'
+
+# Move a file to Trash
+trash () { mv "$@" ~/.Trash ; }
+
+manpdf() {
+  local manpage="$1"
+  (( $# > 0 )) && shift
+  local manargs=( "$@" )
+  man -t "${manargs[@]}" "$manpage" | open -f -a Preview
+}
--- links/bash/mac/sysadmin.bash
+++ links/bash/mac/sysadmin.bash
@@ -2,3 +2,7 @@
 alias battery='pmset -g batt'
 # Start the screensaver
 alias screensaver='open -a ScreenSaverEngine'
+
+getip() {
+  ipconfig getifaddr en0
+}
