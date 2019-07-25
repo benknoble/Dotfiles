06b826679d8a2d12e5fc4efdd83c4b29d5d7a674
David Ben Knoble <ben.knoble+github@gmail.com>
2019-07-24 21:00:07 -0400

    bash: move paths into paths

--- links/bash/mac/paths.bash
+++ links/bash/mac/paths.bash
@@ -3,3 +3,9 @@ pathadd_front /usr/local/lib/ruby/gems/2.6.0/bin
 
 # ruby path
 pathadd_front /usr/local/opt/ruby/bin
+
+SMLNJ="$(brew --prefix)"/smlnj/bin
+if [[ -d "$SMLNJ" ]]; then
+  pathadd "$SMLNJ"
+fi
+unset SMLNJ
--- links/bash/mac/smlnj_path.bash
+++
@@ -1,5 +0,0 @@
-SMLNJ="$(brew --prefix)"/smlnj/bin
-if [[ -d "$SMLNJ" ]]; then
-  pathadd "$SMLNJ"
-fi
-unset SMLNJ
