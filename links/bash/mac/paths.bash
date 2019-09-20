d7f11bf4a5c324fb0950440b8962b707c214733f
D. Ben Knoble <ben.knoble+github@gmail.com>
2019-09-20 11:15:34 -0400

    sml: install from website instead of brew

--- links/bash/mac/paths.bash
+++ links/bash/mac/paths.bash
@@ -4,7 +4,7 @@ pathadd_front /usr/local/lib/ruby/gems/2.6.0/bin
 # ruby path
 pathadd_front /usr/local/opt/ruby/bin
 
-SMLNJ="$(brew --prefix)"/smlnj/bin
+SMLNJ=/usr/local/smlnj/bin
 if [[ -d "$SMLNJ" ]]; then
   pathadd "$SMLNJ"
 fi
