5ce25805a19245d24d29e6d37616d7756c8e0632
D. Ben Knoble <ben.knoble+github@gmail.com>
2019-12-05 09:53:38 -0500

    nvm: only use it if we absolutely need it

--- links/bash/nvm.bash
+++ links/bash/nvm.bash
@@ -1,8 +1,14 @@
-export NVM_DIR=~/.nvm
-if ! [[ -d "$NVM_DIR" ]] ; then mkdir "$NVM_DIR" ; fi
-if [[ -z "$NVM_PATH" ]] ; then
-  NVM_PATH=/usr/local/opt/nvm/nvm.sh
-fi
-if [[ -s "$NVM_PATH" ]] ; then
-  . "$NVM_PATH"
+use_nvm() {
+  export NVM_DIR=~/.nvm
+  if ! [[ -d "$NVM_DIR" ]] ; then mkdir "$NVM_DIR" ; fi
+  if [[ -z "$NVM_PATH" ]] ; then
+    NVM_PATH=/usr/local/opt/nvm/nvm.sh
+  fi
+  if [[ -s "$NVM_PATH" ]] ; then
+    . "$NVM_PATH"
+  fi
+}
+
+if [[ -s ./.nvmrc ]] ; then
+  use_nvm
 fi
