3e05ed16da43303f3db559109396143360ddf492
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-04-10 17:51:52 -0400

    bash: fix pip completion for pip3

--- links/bash/complete.bash
+++ links/bash/complete.bash
@@ -17,7 +17,7 @@ _pip_completion()
     COMP_CWORD=$COMP_CWORD \
     PIP_AUTO_COMPLETE=1 $1 ) )
 }
-complete -o default -F _pip_completion pip
+complete -o default -F _pip_completion pip3
 
 
 # vault completion
