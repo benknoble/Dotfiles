6d89bf737d0faa557e8efaaec56c217e5f87684a
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-06-03 16:15:39 -0400

    zsh: protect against adding nonexistent paths

--- links/zsh/mac/paths.zsh
+++ links/zsh/mac/paths.zsh
@@ -1,16 +1,20 @@
-pathadd_front /usr/local/lib/ruby/gems/2.7.0/bin
-pathadd_front /usr/local/opt/ruby/bin
+[[ /usr/local/lib/ruby/gems/2.7.0/bin ]] && pathadd_front /usr/local/lib/ruby/gems/2.7.0/bin
+[[ /usr/local/opt/ruby/bin ]] && pathadd_front /usr/local/opt/ruby/bin
 
-pathadd /usr/local/smlnj/bin
+[[ /usr/local/smlnj/bin ]] && pathadd /usr/local/smlnj/bin
 
 () {
   local texpath=/usr/local/texlive/2019
-  pathadd_front man "$texpath"/texmf-dist/doc/man
-  pathadd_front info "$texpath"/texmf-dist/doc/info
-  pathadd_front "$texpath"/bin/x86_64-darwinlegacy
+  [[ -d "$texpath" ]] && {
+    pathadd_front man "$texpath"/texmf-dist/doc/man
+    pathadd_front info "$texpath"/texmf-dist/doc/info
+    pathadd_front "$texpath"/bin/x86_64-darwinlegacy
+  }
 }
 
-pathadd /Applications/MacVim.app/Contents/bin
+[[ -d /Applications/MacVim.app/ ]] && pathadd /Applications/MacVim.app/Contents/bin
 
-pathadd /Applications/MATLAB_R2019a.app/bin
-pathadd /Applications/MATLAB_R2019a.app/bin/maci64
+[[ /Applications/MATLAB_R2019a.app/ ]] && {
+  pathadd /Applications/MATLAB_R2019a.app/bin
+  pathadd /Applications/MATLAB_R2019a.app/bin/maci64
+}
