64c9c517dc26f131122d5f0f41b7e91cddadff87
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-08-18 15:16:54 -0400

    bash: fix paths again

--- links/bash/mac/paths.bash
+++ links/bash/mac/paths.bash
@@ -8,29 +8,6 @@ SMLNJ=/usr/local/smlnj/bin
 pathadd "$SMLNJ"
 unset SMLNJ
 
-texpath=/usr/local/texlive/2019
-if [[ -d "$texpath" ]]; then
-  tex_path_fix() {
-    local type="$1"
-    local path="${!type}"
-    local prepend="$2"
-    case "$path" in
-      *"$prepend"* ) true ;;
-      * )
-        if [[ "$type" = PATH ]]; then
-          _pathdebug "$prepend"
-        fi
-        eval export "$type=$prepend:$path"
-        ;;
-    esac
-  }
-  tex_path_fix MANPATH "$texpath"/texmf-dist/doc/man
-  tex_path_fix INFOPATH "$texpath"/texmf-dist/doc/info
-  tex_path_fix PATH "$texpath"/bin/x86_64-darwinlegacy
-  unset tex_path_fix
-fi
-unset texpath
-
 pathadd /Applications/MacVim.app/Contents/bin
 
 pathadd /Applications/MATLAB_R2019a.app/bin
