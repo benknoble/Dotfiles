7c00aac72cb2f7ef0ba8f2b21a42bd146e61eaa6
D. Ben Knoble <ben.knoble+github@gmail.com>
2025-10-02 11:29:35 -0400

    zsh(paths): use brew_prefix instead of /usr/local
    See e054730aa (shellcheck: make source-path Homebrew-path agnostic,
    2025-10-02).

--- links/zsh/mac/env/paths.zsh
+++ links/zsh/mac/env/paths.zsh
@@ -1,11 +1,11 @@
-[[ -d /usr/local/lib/ruby/gems/3.3.0/bin ]] && pathadd_front /usr/local/lib/ruby/gems/3.3.0/bin
-[[ -d /usr/local/opt/ruby/bin ]] && pathadd_front /usr/local/opt/ruby/bin
+[[ -d "$brew_prefix"/lib/ruby/gems/3.3.0/bin ]] && pathadd_front "$brew_prefix"/lib/ruby/gems/3.3.0/bin
+[[ -d "$brew_prefix"/opt/ruby/bin ]] && pathadd_front "$brew_prefix"/opt/ruby/bin
 
-[[ -d /usr/local/smlnj/bin ]] && pathadd /usr/local/smlnj/bin
+[[ -d "$brew_prefix"/smlnj/bin ]] && pathadd "$brew_prefix"/smlnj/bin
 
 [[ -d /Applications/MacVim.app/ ]] && pathadd /Applications/MacVim.app/Contents/bin
 
-[[ -d /usr/local/opt/openjdk/ ]] && {
-  pathadd_front /usr/local/opt/openjdk/bin
-  pathadd_front man /usr/local/opt/openjdk/libexec/openjdk.jdk/Contents/Home/man
+[[ -d "$brew_prefix"/opt/openjdk/ ]] && {
+  pathadd_front "$brew_prefix"/opt/openjdk/bin
+  pathadd_front man "$brew_prefix"/opt/openjdk/libexec/openjdk.jdk/Contents/Home/man
 }
