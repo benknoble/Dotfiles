ec3f72fcb52937e6b4d4d730161b319090b3ff6e
D. Ben Knoble <ben.knoble+github@gmail.com>
2025-10-02 11:36:15 -0400

    nvm: unhardcode brew_prefix
    See e054730aa (shellcheck: make source-path Homebrew-path agnostic,
    2025-10-02).

--- links/bash/nvm.bash
+++ links/bash/nvm.bash
@@ -1,14 +1,16 @@
 use_nvm() {
+  local brew_prefix
+  brew_prefix="$(cat ~/.brew-prefix 2>/dev/null || brew --prefix | tee ~/.brew-prefix)"
   export NVM_DIR=~/.nvm
   if ! [[ -d "$NVM_DIR" ]] ; then mkdir "$NVM_DIR" ; fi
   if [[ -z "$NVM_PATH" ]] ; then
-    NVM_PATH=/usr/local/opt/nvm/nvm.sh
+    NVM_PATH="$brew_prefix"/opt/nvm/nvm.sh
   fi
   if [[ -s "$NVM_PATH" ]] ; then
     . "$NVM_PATH"
   fi
-  if [[ -s /usr/local/etc/bash_completion.d/nvm ]]; then
-    . /usr/local/etc/bash_completion.d/nvm
+  if [[ -s "$brew_prefix"/etc/bash_completion.d/nvm ]]; then
+    . "$brew_prefix"/etc/bash_completion.d/nvm
   fi
 }
 
