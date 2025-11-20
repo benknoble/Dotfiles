21d476068550ebfdd522587c4a45b863441f2c46
D. Ben Knoble <ben.knoble+github@gmail.com>
2025-11-20 18:33:44 -0500

    shell: attempt to make use_nvm brew-agnostic
    I haven't actually tested this because I'm resisting installing nvm on
    my new machine, but I don't see why it wouldn't work
    
    Best-viewed-with: --ignore-space-change

--- links/bash/nvm.bash
+++ links/bash/nvm.bash
@@ -1,18 +1,26 @@
-use_nvm() {
-  local brew_prefix
-  brew_prefix="$(cat ~/.brew-prefix 2>/dev/null || brew --prefix | tee ~/.brew-prefix)"
-  export NVM_DIR=~/.nvm
-  if ! [[ -d "$NVM_DIR" ]] ; then mkdir "$NVM_DIR" ; fi
-  if [[ -z "$NVM_PATH" ]] ; then
-    NVM_PATH="$brew_prefix"/opt/nvm/nvm.sh
-  fi
-  if [[ -s "$NVM_PATH" ]] ; then
-    . "$NVM_PATH"
-  fi
-  if [[ -s "$brew_prefix"/etc/bash_completion.d/nvm ]]; then
-    . "$brew_prefix"/etc/bash_completion.d/nvm
-  fi
-}
+if command -v brew >/dev/null 2>&1; then
+  use_nvm() {
+    local brew_prefix
+    brew_prefix="$(cat ~/.brew-prefix 2>/dev/null || brew --prefix | tee ~/.brew-prefix)"
+    export NVM_DIR=~/.nvm
+    if ! [[ -d "$NVM_DIR" ]] ; then mkdir "$NVM_DIR" ; fi
+    if [[ -z "$NVM_PATH" ]] ; then
+      NVM_PATH="$brew_prefix"/opt/nvm/nvm.sh
+    fi
+    if [[ -s "$NVM_PATH" ]] ; then
+      . "$NVM_PATH"
+    fi
+    if [[ -s "$brew_prefix"/etc/bash_completion.d/nvm ]]; then
+      . "$brew_prefix"/etc/bash_completion.d/nvm
+    fi
+  }
+else
+  use_nvm() {
+    export NVM_DIR="${XDG_CONFIG_HOME:-$HOME}/.nvm"
+    [[ -s "$NVM_DIR/nvm.sh" ]] && . "$NVM_DIR/nvm.sh"
+    [[ -s "$NVM_DIR/bash_completion" ]] && . "$NVM_DIR/bash_completion"
+  }
+fi
 
 if [[ -s ./.nvmrc ]] ; then
   use_nvm
