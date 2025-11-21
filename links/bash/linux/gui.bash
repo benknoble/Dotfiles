03bb9484b78a2e2c002dfd0ac7a7c760645eed83
D. Ben Knoble <ben.knoble+github@gmail.com>
2025-11-20 19:05:57 -0500

    alert: fix for Gentoo icons

--- links/bash/linux/gui.bash
+++ links/bash/linux/gui.bash
@@ -1,11 +1,11 @@
 alert() {
   local exit=$?
-  local icon=terminal
+  local icon=utilities-terminal
   local subj="${1:-Alert}"
   (($# > 0)) && shift
   local msg="$*"
   if ((exit != 0)) ; then
-    icon=error
+    icon=dialog-error
   fi
   notify-send --urgency=low -i "$icon" "$subj" "$msg"
 }
